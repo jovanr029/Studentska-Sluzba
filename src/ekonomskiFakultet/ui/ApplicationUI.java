b95491b538a2f45906eb8ed0ea82a5c473d52f27
jovanr029 <jradosavkic1@gmail.com>
2020-12-23 22:29:05 +0100

    Treci komit

--- src/ekonomskiFakultet/ui/ApplicationUI.java
+++ src/ekonomskiFakultet/ui/ApplicationUI.java
@@ -63,9 +63,7 @@ public class ApplicationUI {
 		System.out.println("\tOpcija broj 5 - Rad sa pohadjanjem predmeta");
 		System.out.println("\tOpcija broj 6 - Rad sa predavanjem profesora");
 		System.out.println("\tOpcija broj 7 - Rad sa ispitnim prijavama studenta");
-		System.out.println("\tOpcija broj 7 - Rad sa ispitnim prijavama studenta");
-		System.out.println("\tOpcija broj 7 - Rad sa ispitnim prijavama studenta");
-		System.out.println("\t\t ...");
+		System.out.println("\tOpcija broj 8 - Isprobaj git");
 		System.out.println("\tOpcija broj 0 - IZLAZ IZ PROGRAMA");
 	}
 }
